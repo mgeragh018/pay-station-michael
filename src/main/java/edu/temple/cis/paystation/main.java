145a274e34a6400e92310eb2a68c486c29409dcc
Michael Geraghty <tup13771@temple.edu>
2024-09-20 11:35:21 -0400

    Create a main.java file. This file will be used to simulate software.

---
+++ src/main/java/edu/temple/cis/paystation/main.java
@@ -0,0 +1,13 @@
+//this is the file we will use for our main program
+
+package edu.temple.cis.paystation;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+
+
